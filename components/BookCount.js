898bb98f4333d5a9035d16ae96c1be1509818984
Mayank Dhote <mrmayankdhote@email.com>
2024-03-05 04:12:05 -0800

    Update BookCount Js to Class compent to Functional compent

--- components/BookCount.js
+++ components/BookCount.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { View ,Text} from "react-native";
+import { View, Text } from "react-native";
+import PropTypes from "prop-types"; // ES6
 
-export default class BookCount extends React.Component {
-  render() {
-    return (
-        <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-          }}
-        >
-          {this.props.title}
-        </Text>
-        <Text> {this.props.count}</Text>
-      </View> 
-    );
-  }
-}
+const BookCount = ({ title, count }) => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 20,
+      }}
+    >
+      {title}
+    </Text>
+    <Text> {count}</Text>
+  </View>
+);
+
+BookCount.prototype = {
+  count: PropTypes.number.isRequired,
+  title: PropTypes.string,
+};
+
+BookCount.defaultProps = {
+  title: "Title",
+};
+export default BookCount;
