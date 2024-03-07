0a116482960a613a0879793aac7a29e55d16c719
Mayank Dhote <mrmayankdhote@email.com>
2024-03-07 00:16:41 -0800

    internal fix

--- BookWorm.js
+++ BookWorm.js
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,14 +7,38 @@ import WelcomeScreen from "./AppSwitchNavigator/WelcomeScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import colors from "./assets/color";
 import HomeScreen from "./screens/HomeScreen";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 
 export default class BookWorm extends React.Component {
   render() {
     const Stack = createStackNavigator();
+
+    const Drawer = createDrawerNavigator();
+
+    function LoginStack() {
+      return (
+        <Drawer.Navigator>
+          <Drawer.Screen
+            name="HomeScreen"
+            options={{
+              title:'Home',
+              drawerStyle: {
+                backgroundColor: "#red",
+                width: 240,
+              },
+              drawerIcon: ({color, size}) => (
+            <Ionicons
+               name='home' size={24} color={colors.bgMain}
+            />
+          ),
+            }}
+            component={HomeScreen}
+          />
+        </Drawer.Navigator>
+      );
+    }
 
     return (
       <NavigationContainer>
@@ -41,9 +65,9 @@ export default class BookWorm extends React.Component {
             options={{ headerShown: true }}
           />
           <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{ headerShown: true }}
+            name="LoginStack"
+            component={LoginStack}
+            options={{ headerShown: false }}
           />
         </Stack.Navigator>
       </NavigationContainer>
