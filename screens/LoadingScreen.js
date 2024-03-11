75132c94ea370178eed5106415e28dd94ad0053a
Mayank Dhote <mrmayankdhote@email.com>
2024-03-11 23:49:12 +0530

    internal issue dix

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -5,6 +5,7 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 class LoadingScreen extends React.Component {
@@ -12,28 +13,36 @@ class LoadingScreen extends React.Component {
     this.checkIfLoggedIn();
   };
 
+
   checkIfLoggedIn = () => {
     try {
+    
       const auth = getAuth();
       this.unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) { 
-          this.props.navigation.navigate('LoginStack',{
-            user:user
-          });
+        if (user) {
+         const _storeData = async (user) => {
+            try {
+              await AsyncStorage.setItem("user", JSON.stringify(user));
+              this.props.navigation.navigate("LoginStack");
+            } catch (error) {
+              alert(error)
+              // Error saving data
+            }
+          };
+    
+         _storeData(user);
         } else {
-          this.props.navigation.navigate('LoginScreen')
-
+          this.props.navigation.navigate("LoginScreen");
         }
       });
     } catch (error) {
-      alert(error)
-     
+      alert(error);
     }
   };
 
   componentWillUnmount = () => {
     this.unsubscribe();
-  }
+  };
   render() {
     return <View />;
   }
